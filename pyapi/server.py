19cd78db440b8a3000d1fc9088fd2b45f52c2982
Alin Chiuaru <alin.chiuaru@gmail.com>
2015-01-11 20:33:53 +0200

    Added posts CRUD

--- pyapi/server.py
+++ pyapi/server.py
@@ -18,7 +18,6 @@ mysql.init_app(app)
 
 @app.route('/')
 def hello():
-    #return 'hello world flask dssd'
     return render_template('index.html')
 
 @app.route("/Authenticate")
@@ -26,7 +25,7 @@ def Authenticate():
     username = request.args.get('UserName')
     password = request.args.get('Password')
     cursor = mysql.connect().cursor()
-    cursor.execute("SELECT * from users where username='" + username + "' and password='" + password + "'")
+    cursor.execute("SELECT * from users WHERE username='" + username + "' and password='" + password + "'")
     data = cursor.fetchone()
     if data is None:
      return "Username or Password is wrong"
@@ -36,8 +35,18 @@ def Authenticate():
 @app.route('/api/users/<user_id>', methods=['GET'])
 def getUser(user_id):
     cursor = mysql.connect().cursor(MySQLdb.cursors.DictCursor)
-    cursor.execute(" SELECT * from users where id='" + user_id + "'")
+    query1 = "SELECT * from users WHERE id='" + user_id + "'"
+    query2 = "SELECT id from posts WHERE creator_id='" + user_id + "'"
+    # querytest = "SELECT * from users u INNER JOIN posts p on u.id = p.creator_id WHERE u.id = '" + user_id + "'"
+    # cursor.execute(" SELECT * from users WHERE id='" + user_id + "'")
+    cursor.execute(query1)
     user = cursor.fetchone()
+    cursor.execute(query2)
+    userPosts = cursor.fetchall()
+    postsList = ()
+    for post in userPosts:
+        postsList = postsList + (post['id'],)
+    user['posts'] = postsList
     return jsonify(user)
     
 @app.route('/api/users/', methods=['GET'])
@@ -50,7 +59,98 @@ def getUsers():
         mylist[row['id']] = row
     return jsonify(mylist)
 
+@app.route('/api/users/', methods=['POST'])
+def insertUser():
+    data = {}
+    data["username"] = request.form['username']
+    data["password"] = request.form['password']
+    conn = mysql.connect()
+    cursor = conn.cursor(MySQLdb.cursors.DictCursor)
+    query = "INSERT INTO users(username, password)" +\
+        "VALUES ('%s', '%s')"
+    cursor.execute(query % (data["username"], data["password"]))
+    conn.commit()
+    return jsonify(data)
 
+@app.route('/api/users/<user_id>', methods=['DELETE'])
+def removeUser(user_id):
+    conn = mysql.connect()
+    cursor = conn.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute("DELETE FROM users WHERE id='" + user_id + "'")
+    conn.commit()
+    return "User " + user_id + " has been deleted successfully"
+
+@app.route('/api/users/<user_id>', methods=['PUT'])
+def updateUser(user_id):
+    data = {}
+    data["username"] = request.form['username']
+    data["password"] = request.form['password']
+    conn = mysql.connect()
+    cursor = conn.cursor(MySQLdb.cursors.DictCursor)
+    query = " UPDATE users SET username='%s', password='%s' WHERE id='" + user_id + "'"
+    cursor.execute(query % (data["username"], data["password"]))
+    conn.commit()
+    return jsonify(data)
+
+
+# POSTS
+@app.route('/api/posts/<post_id>', methods=['GET'])
+def getPost(post_id):
+    cursor = mysql.connect().cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute(" SELECT * from posts WHERE id='" + post_id + "'")
+    post = cursor.fetchone()
+    return jsonify(post)
+    
+@app.route('/api/posts/', methods=['GET'])
+def getPosts():
+    mylist = {}
+    cursor = mysql.connect().cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute("SELECT * from posts")
+    rows = cursor.fetchall()
+    for row in rows:
+        mylist[row['id']] = row
+    return jsonify(mylist)
+
+# @app.route('/api/posts/<user_id>', methods=['GET'])
+# def getUserPost(user_id):
+#     cursor = mysql.connect().cursor(MySQLdb.cursors.DictCursor)
+#     cursor.execute(" SELECT * from posts WHERE creator_id='" + user_id + "'")
+#     post = cursor.fetchone()
+#     return jsonify(post)
+
+@app.route('/api/posts/', methods=['POST'])
+def insertPost():
+    data = {}
+    data["title"] = request.form['title']
+    data["text"] = request.form['text']
+    data["creator_id"] = request.form['creator_id']
+    conn = mysql.connect()
+    cursor = conn.cursor(MySQLdb.cursors.DictCursor)
+    query = "INSERT INTO posts(title, text, creator_id)" +\
+        "VALUES ('%s', '%s', '%s')"
+    cursor.execute(query % (data["title"], data["text"], data["creator_id"]))
+    conn.commit()
+    return jsonify(data)
+
+@app.route('/api/posts/<post_id>', methods=['DELETE'])
+def removePost(post_id):
+    conn = mysql.connect()
+    cursor = conn.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute("DELETE FROM posts WHERE id='" + post_id + "'")
+    conn.commit()
+    return "Post " + post_id + " has been deleted successfully"
+
+@app.route('/api/posts/<post_id>', methods=['PUT'])
+def updatePost(post_id):
+    data = {}
+    data["title"] = request.form['title']
+    data["text"] = request.form['text']
+    conn = mysql.connect()
+    cursor = conn.cursor(MySQLdb.cursors.DictCursor)
+    query = " UPDATE posts SET title='%s', text='%s' WHERE id='" + post_id + "'"
+    cursor.execute(query % (data["title"], data["text"]))
+    conn.commit()
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
--- pyapi/static/js/app.js
+++ pyapi/static/js/app.js
@@ -1,7 +1,96 @@
-$.ajax({
-	type: 'GET',
-	url: 'api/users/',
-	success: function(data) {
-		console.log(data);
-	}
+$(function() {
+	
+
+	$('#sendReq').on('click', function() {
+		var user = {
+			username: $('#username').val(),
+			password: $('#password').val()
+		};
+		
+		$.ajax({
+			type: 'POST',
+			url: '/api/users/',
+			data: user,
+			success: function(res) {
+				console.log(res);
+			}
+		});
+	});
+
+	$('#deleteReq').on('click', function() {
+		var deleteID = parseInt($('#delete').val());
+
+		$.ajax({
+			type: 'DELETE',
+			url: '/api/users/' + deleteID,
+			success: function(res) {
+				console.log('del:', res);
+			}
+		});
+
+	});
+
+	$('#updateReq').on('click', function() {
+		var user = {
+			id: $('#uid').val(),
+			username: $('#uname').val(),
+			password: $('#upas').val()
+		};
+
+		$.ajax({
+			type: 'PUT',
+			url: '/api/users/' + user.id,
+			data: user,
+			success: function(res) {
+				console.log('update:',res);
+			}
+		})
+	});
+
+	$('#sendPostReq').on('click', function() {
+		var post = {
+			title: $('#title').val(),
+			text: $('#text').val(),
+			creator_id: $('#creator').val()
+		}
+
+		$.ajax({
+			type: 'POST',
+			url: 'api/posts/',
+			data: post,
+			success: function(res) {
+				console.log('new post:', res);
+			}
+		})
+	});
+
+	$('#deletePostReq').on('click', function() {
+		var deleteID = parseInt($('#deletePost').val());
+
+		$.ajax({
+			type: 'DELETE',
+			url: '/api/posts/' + deleteID,
+			success: function(res) {
+				console.log('delete post:', res);
+			}
+		});
+
+	});
+
+	$('#updatePostReq').on('click', function() {
+		var post = {
+			id: $('#pid').val(),
+			title: $('#ptitle').val(),
+			text: $('#ptext').val()
+		};
+
+		$.ajax({
+			type: 'PUT',
+			url: '/api/posts/' + post.id,
+			data: post,
+			success: function(res) {
+				console.log('update post:',res);
+			}
+		})
+	});
 });
